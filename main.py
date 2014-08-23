3a22467f19426bcb0e2dbf595107a3ac09fee6a9
Alexander Ryzhkov <sash13@bigmir.net>
2014-08-23 23:04:20 +0300

    Transfer to kv

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from kivy.graphics import Color, Rectangle
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
-
+from kivy.utils import get_color_from_hex as C
 
 texts = ["Деды \nвоевали", "Гейропа", "Бендеровцы", "Крымнаш", "Салоеды",
          "...не\nпоставить\nна колени", "Укропы", "Майданутые", "Чурки",
@@ -17,51 +17,19 @@ texts = ["Деды \nвоевали", "Гейропа", "Бендеровцы", 
          "Порядок", "Стабильность", "Жидо\nфашисты", "Обезьяна\nчерножопая",
          "Нацисты", "Загниваюшая\nЕвропа", "Великая\nРусь"]
 
+class MainApp(App):
 
-class RootWidget(FloatLayout):
-
-    def __init__(self, **kwargs):
+    def build(self):
         self.index = [0]*25
-        # make sure we aren't overriding any important functionality
-        super(RootWidget, self).__init__(**kwargs)
-        # let's add a Widget to this layout
-
-        self.layout = GridLayout(padding=20, cols=5,
-                                 pos_hint={'center_y': 0.3})
-
         self.bingo = texts[12]
-        for i in range(5*5):
-            btn = Button(
-                text=texts[i],
-                size=(100, 100),
-                size_hint=(None, None),
-                # markup=True,
-                halign='center',
-                # color = [0,0,0,1],
-                # background_color = [1,1,1,1],
-                border = [20, 20, 20, 20],
-                # background_normal = ""
-                # pos_hint={'center_x': .5, 'center_y': .5}
-            )
+        self.game_end = 0
+        for (key, val) in self.root.ids.items():
+            if key[0] == 'b':
+		i = int(key.split('b')[-1])
+		val.text = texts[i-1]
 
-            btn.bind(on_press=self.callback)
-            self.layout.add_widget(btn)
-            btn = Button(
-                text="Reset",
-                halign='center',
-                size=(150, 150),
-                pos_hint={'x': 0.36, 'center_y': .1},
-                size_hint=(None, None)
-                # border = [16,16,16,16]
-            )
-        btn.bind(on_press=self.reset)
 
-        self.add_widget(self.layout)
-        self.add_widget(btn)
-
-        # self.add_widget(rootLayout)
-
-    def callback(self, value):
+    def press(self, value):
         if value.text is not self.bingo:
             value.background_color = [0, 1, 1, 1]
             self.index[texts.index(value.text)] = 1
@@ -75,11 +43,13 @@ class RootWidget(FloatLayout):
                     ii += self.index[j*5+i]
 
                 if ii is 5 or jj is 5:  # check v/h line
-                    self.layout.children[12].background_color = [0, 1, 1, 1]
+                    self.root.ids.b13.background_color = [0, 1, 1, 1]
+		    self.game_end = 1
                 if i is 2:
                     if ii is 4 or jj is 4:  # check mediana
-                        self.layout.children[12].background_color = \
+                        self.root.ids.b13.background_color = \
                             [0, 1, 1, 1]
+			self.game_end = 1
                 ii = 0
                 jj = 0
             k = 0
@@ -89,24 +59,18 @@ class RootWidget(FloatLayout):
                 k += 1
 
             if ii is 4 or jj is 4:  # check main diagonal
-                self.layout.children[12].background_color = [0, 1, 1, 1]
+                self.root.ids.b13.background_color = [0, 1, 1, 1]
+		self.game_end = 1
+	    if self.game_end:
+		self.root.ids.info_label.text = 'BINGO!'
+		self.game_end = 0
 
-    def reset(self, pok):
-        for child in self.layout.children:
-            child.background_color = [1, 1, 1, 1]
-            self.index = [0]*25
-
-
-class MainApp(App):
-
-    def build(self):
-        self.root = root = RootWidget()
-        root.bind(size=self._update_rect, pos=self._update_rect)
-
-        with root.canvas.before:
-            Color(0, 0, 0, 1)
-            self.rect = Rectangle(size=root.size, pos=root.pos)
-        return root
+    def reset(self):
+        for (key, val) in self.root.ids.items():
+            if key[0] == 'b':
+                val.background_color = C('#2ecc71')
+        self.index = [0]*25
+	self.root.ids.info_label.text = ''
 
     def _update_rect(self, instance, value):
         self.rect.pos = instance.pos
